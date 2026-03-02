af944fe84a0157692285517c6ce1198a3e6ca6da
Andres Mejia <160779799+andresemejia@users.noreply.github.com>
2026-03-01 20:35:50 -0500

    Update check_bronze.crm_prd_info.sql

--- scripts/silver/check_bronze.crm_prd_info.sql
+++ scripts/silver/check_bronze.crm_prd_info.sql
@@ -1,3 +1,4 @@
+-- Quality Checks
 -- Check For Nulls or Duplicates in Primary Key
 -- Expectation: No Result
 
@@ -7,3 +8,32 @@ SELECT
 FROM bronze.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for unwanted Spaces
+-- Expectation: No Results
+
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Data Standardization & Consistency
+
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+SELECT * FROM bronze.crm_cust_info
+
+-- Check for NULLs or Negative Numbers
+-- Expectation: No results
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--- Check for Invalid Date Orders
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+--- FINAL LOOK 
+SELECT *
+FROM bronze.crm_prd_info
